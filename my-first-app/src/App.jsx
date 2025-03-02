8864187930413bfec0d8f548f4a72e513140d296
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-03-02 21:01:49 +0530

    learning

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {}
+
+const app1 = () => {
+  return (
+    <h2>Hello My name is Birat Roka</h2>
+  )
+}
+
+export default App
