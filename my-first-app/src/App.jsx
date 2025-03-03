50a3403e33097d07382376c78811067987a6fbd9
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-03-03 22:41:21 +0530

    learning tailwind

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -3,11 +3,37 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {}
-
-const app1 = () => {
+const Card = ({ title, content }) => {
   return (
-    <h2>Hello My name is Birat Roka</h2>
+    <div class= "card" style={{
+      border: '1px solid black', 
+      padding: '20px', 
+      margin: '10px',
+      width: '300px',
+      display: 'inline-block',
+      backgroundColor: 'lightblue',
+      borderRadius: '10px',
+      minHeight: '200px',
+      boxShadow: '5px 5px 5px 5px lightgrey'}}>
+
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  )   
+   
+}
+  
+
+const App  = () => {
+  return (
+    <div className="card-container">
+    
+
+
+    <Card title="The lion king" content="Animated Movie" />
+    <Card title="Avatar" content="Animated Movie" />
+    
+    </div>
   )
 }
 
