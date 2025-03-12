5103a6df7f1053cb083809864591b9d494282455
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-03-12 23:01:13 +0530

    update app.jsx

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './components/search.jsx'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <main>
       <div className='pattern'/>
@@ -9,8 +10,9 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-          <Search />
         </header>
+          <Search  searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
       </div>
     </main>
   )
