26a9c72526a6546dfd6b1512998ea6b2249399f8
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-03-12 23:27:13 +0530

    update the search menu

--- my-first-app/src/components/search.jsx
+++ my-first-app/src/components/search.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react'
 
-const Search = ({searchTerm, setSearchTerm}) => {
-    return (
-        <div className="Search">
-            <div>
-                <img src="./search.png" alt="Search Icon" />
-                <input 
-                type="text"
-                placeholder="Search for a movie..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                />
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.png" alt="search" />
 
-            </div>
-        </div>
-    )
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  )
 }
-export default Search;
+export default Search
